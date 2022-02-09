9c14a483bcd24e0911c715b9642dee48c9312a48
Saichandhar <tsaichandharrreddy@gmail.com>
2022-02-08 18:22:31 -0600

    Initial Commit

---
+++ spotify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  spotify
+//
+//  Created by Thodati,Sai Chandhar Reddy on 2/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
