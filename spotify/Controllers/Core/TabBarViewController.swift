de2a0b20fedfd7af5bd0dcd99343bc01f74ab10b
Saichandhar <tsaichandharrreddy@gmail.com>
2022-02-08 20:08:36 -0600

    committing first time

---
+++ spotify/Controllers/Core/TabBarViewController.swift
@@ -0,0 +1,12 @@
+//
+//  TabBarViewController.swift
+//  spotify
+//
+//  Created by Thodati,Sai Chandhar Reddy on 2/8/22.
+//
+
+import UIKit
+
+class TabBarViewController: UITableBarController {
+
+}
